6c405e29486c5f7b8647e35697589449291cb8f2
Aakash Subedi <asubedi2@umbc.edu>
2025-12-09 12:10:25 -0500

    fix import syntax

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const axios = require('axios');
-import { OpenAI } from "openai";
+const OpenAI = require('openai')
 
 const app = express();
 
